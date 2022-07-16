32b4fe939186118fca6d4a6a167578aa020cdee8
unkler <poru@add-natural.com>
2022-07-16 15:01:13 +0900

    記事削除処理を追加

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -40,4 +40,11 @@ class ArticleController extends Controller
 
         return redirect()->route('articles.index');
     }
+
+    public function destroy(Article $article)
+    {
+        $article->delete();
+
+        return redirect()->route('articles.index');
+    }
 }
